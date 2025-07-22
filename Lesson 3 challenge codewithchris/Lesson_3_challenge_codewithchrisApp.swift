837ccd16b148e20e2a07c4bb0d39662021cf9788
SpiritAndPollen <jrtt212@gmail.com>
2025-07-23 05:34:52 +1000

    Initial Commit

---
+++ Lesson 3 challenge codewithchris/Lesson_3_challenge_codewithchrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_3_challenge_codewithchrisApp.swift
+//  Lesson 3 challenge codewithchris
+//
+//  Created by Jedda Tuuta on 23/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_3_challenge_codewithchrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
